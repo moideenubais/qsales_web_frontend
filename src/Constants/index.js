38c79a3642333ec2ae43a3459d5628f39c9536e4
rameezamem1 <rameeza.memon1@gmail.com>
2022-04-16 17:49:42 +0500

    terms and condition page updated

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1237,7 +1237,103 @@ Offerings.<br/><br/>
 
 
 </div>
-    `
+`,
+infringementPolicy:`
+<div class=WordSection1>
+<p class=MsoBodyText style='margin-top:4.0pt;margin-right:4.15pt;margin-bottom:
+0in;margin-left:5.0pt;margin-bottom:.0001pt;line-height:187%'>Qsales 2022 is
+committed to removing infringing materials from the site. It is in Qsales
+2022’s interest to ensure that infringing items are removed from the site.</p>
+<p class=MsoBodyText style='margin-left:0in'><span style='font-size:15.5pt'>&nbsp;</span></p>
+<h1>Policy</h1>
+<p class=MsoBodyText style='margin-top:8.9pt;margin-right:4.15pt;margin-bottom:
+0in;margin-left:5.0pt;margin-bottom:.0001pt;line-height:187%'>Qsales 2022
+respects third party Intellectual Property rights and actively supports protection
+of all third party Intellectual Property including Copyrights and Trademarks
+(“IP”). It is our policy to expeditiously respond to clear notices of alleged
+IP</p>
+<p class=MsoBodyText style='margin-left:5.0pt;line-height:13.6pt'>infringement.</p>
+<p class=MsoBodyText style='margin-left:0in'><span style='font-size:13.0pt'>&nbsp;</span></p>
+<p class=MsoBodyText style='margin-top:9.15pt;margin-right:4.15pt;margin-bottom:
+0in;margin-left:5.0pt;margin-bottom:.0001pt;line-height:187%'>If we receive
+proper notification of IP infringement, our response to such notices will
+include removing or disabling access to material claimed to be the subject of</p>
+<p class=MsoBodyText style='margin-left:5.0pt;line-height:13.65pt'>infringing
+activity.</p>
+<p class=MsoBodyText style='margin-left:0in'><span style='font-size:13.0pt'>&nbsp;</span></p>
+<p class=MsoBodyText style='margin-top:.15pt;margin-right:0in;margin-bottom:
+0in;margin-left:0in;margin-bottom:.0001pt'><span style='font-size:13.0pt'>&nbsp;</span></p>
+<h1>How to report a listing</h1>
+<p class=MsoBodyText style='margin-top:8.9pt;margin-right:8.75pt;margin-bottom:
+0in;margin-left:5.0pt;margin-bottom:.0001pt;line-height:186%'>If you have a
+good faith belief that your IP right has been infringed on the site, you may
+follow the below process: We require that the Intellectual Property right owner
+or authorized agent provide the following details and email it to ******** .The
+email should contain the below information.</p>
+<p class=MsoBodyText style='margin-top:.2pt;margin-right:0in;margin-bottom:
+0in;margin-left:0in;margin-bottom:.0001pt'><span style='font-size:10.5pt'>&nbsp;</span></p>
+<p class=MsoListParagraph style='margin-right:13.0pt;line-height:115%'><span
+style='font-size:12.0pt;line-height:115%;letter-spacing:-.25pt'>1.<span
+style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp; </span></span><span
+style='font-size:12.0pt;line-height:115%'>Identification or description of the
+copyrighted work/ trademark that has <span style='letter-spacing:-.25pt'>been </span>infringed.</span></p>
+<p class=MsoListParagraph><span style='font-size:12.0pt;letter-spacing:-.25pt'>2.<span
+style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp; </span></span><span
+style='font-size:12.0pt'>Clear identification or description of where the
+material that you claim is</span></p>
+<p class=MsoBodyText style='margin-top:2.05pt'><a
+href="http://www.classicaldrive.com/"><span style='color:windowtext;text-decoration:
+none'>infringing is located on www.classicaldrive.com</span></a> with adequate
+particulars.</p>
+<p class=MsoListParagraph style='margin-top:2.1pt;margin-right:19.0pt;
+margin-bottom:0in;margin-left:41.0pt;margin-bottom:.0001pt;line-height:115%'><span
+style='font-size:12.0pt;line-height:115%;letter-spacing:-.25pt'>3.<span
+style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp; </span></span><span
+style='font-size:12.0pt;line-height:115%'>Exact link (URL) (in case of
+copyright infringement) (Note: Qsales 2022 is unable to process requests which
+do not specify exact URLs. Please do <span style='letter-spacing:-.3pt'>not </span>provide
+links to browse pages or links of search queries as these pages are dynamic and
+their contents change with time).</span></p>
+<p class=MsoListParagraph><span style='font-size:12.0pt;letter-spacing:-.25pt'>4.<span
+style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp; </span></span><span
+style='font-size:12.0pt;letter-spacing:-.3pt'>Your </span><span
+style='font-size:12.0pt'>address, telephone number, and email<span
+style='letter-spacing:.2pt'> </span>address.</span></p>
+<p class=MsoListParagraph style='margin-top:2.05pt;margin-right:44.95pt;
+margin-bottom:0in;margin-left:41.0pt;margin-bottom:.0001pt;text-align:justify;
+text-justify:inter-ideograph;line-height:115%'><span style='font-size:12.0pt;
+line-height:115%;letter-spacing:-.25pt'>5.<span style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp;
+</span></span><span style='font-size:12.0pt;line-height:115%'>A statement by
+you that you have a good faith belief that the use of <span style='letter-spacing:
+-.3pt'>the </span>material complained of is not authorized by the copyright or
+intellectual property <span style='letter-spacing:-.15pt'>owner, </span>its
+agent, or the<span style='letter-spacing:.15pt'> </span><span style='letter-spacing:
+-.2pt'>law.</span></span></p>
+<p class=MsoListParagraph style='margin-right:7.65pt;line-height:115%'><span
+style='font-size:12.0pt;line-height:115%;letter-spacing:-.25pt'>6.<span
+style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp; </span></span><span
+style='font-size:12.0pt;line-height:115%'>A statement by you, that the
+information in your notice is accurate and that you are the copyright or
+intellectual property owner or authorized to act on <span style='letter-spacing:
+-.3pt'>the </span>copyright or intellectual property owner’s behalf.</span></p>
+</div>
+<span style='font-size:12.0pt;line-height:115%;font-family:"Arial",sans-serif'><br
+clear=all style='page-break-before:always'>
+</span>
+<div class=WordSection2>
+<p class=MsoListParagraph style='margin-top:4.0pt'><span style='font-size:12.0pt;
+letter-spacing:-.25pt'>7.<span style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp;
+</span></span><span style='font-size:12.0pt'>Brand Name (in case of Trademark<span
+style='letter-spacing:-.3pt'> </span>infringement)</span></p>
+<p class=MsoListParagraph style='margin-top:2.05pt;margin-right:5.0pt;
+margin-bottom:0in;margin-left:41.0pt;margin-bottom:.0001pt;line-height:115%'><span
+style='font-size:12.0pt;line-height:115%;letter-spacing:-.25pt'>8.<span
+style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp; </span></span><span
+style='font-size:12.0pt;line-height:115%'>Details of the intellectual property
+being infringed (Provide copyrighted <span style='letter-spacing:-.15pt'>images
+</span>or trademark certificates as attachments)</span></p>
+</div>
+`
 }
 
 export default Strings
